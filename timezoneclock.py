610dabbfb08d82ee0474b89ad749c780a83e68b3
dyedquartz <dyedquartz@gmail.com>
2018-04-22 08:41:51 +0500

    pulling my hair out with async functions failing

--- timezoneclock.py
+++ timezoneclock.py
@@ -21,9 +21,14 @@ GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 ssegdisplay = sseg.SevenSegment(address=0x70)
 alphadisplay = alphanum.AlphaNum4(address=0x71)
 
-# Begins display
+# Begin and reset display
 ssegdisplay.begin()
+ssegdisplay.clear()
+ssegdisplay.write_display()
+
 alphadisplay.begin()
+alphadisplay.clear()
+alphadisplay.write_display()
 
 # Loop for starting displays
 async def settime():
@@ -32,19 +37,22 @@ async def settime():
     hour = utctime.hour + offset
     minute = utctime.minute
     second = utctime.second
-
+    
     # Clear Display
     ssegdisplay.clear()
-
+    
     ssegdisplay.set_colon(second % 2)
+    
+    ssegdisplay.write_display()
+    
     await asyncio.sleep(0.25)
 
 async def buttons():
     if GPIO.input(18) == False:
         print('Left button pressed')
-        await asyncio.sleep(0.02)
+        await asyncio.sleep(0.1)
 
 asyncloop = asyncio.get_event_loop()
 tasks = [asyncloop.create_task(settime()), asyncloop.create_task(buttons())]
-wait_tasks = asyncio.wait(tasks)
+#wait_tasks = asyncio.wait(tasks)
 asyncloop.run_forever()
