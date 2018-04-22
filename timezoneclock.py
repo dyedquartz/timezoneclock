8f63a00d5ddd472c65b2f5a2be7bdd7827225dfd
dyedquartz <dyedquartz@gmail.com>
2018-04-22 07:38:45 +0500

    fixing python shenanigans part1

--- timezoneclock.py
+++ timezoneclock.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 from datetime import datetime
-from Adafruit_LED_Backpack import SevenSegment as 7seg
+from Adafruit_LED_Backpack import SevenSegment as sseg
 from Adafruit_LED_Backpack import AlphaNum4 as alphanum
 import os
 import time
@@ -18,15 +18,15 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Creates displays
-7segdisplay = 7seg.SevenSegment(address=0x70)
+ssegdisplay = sseg.SevenSegment(address=0x70)
 alphadisplay = alphanum.AlphaNum4(address=0x71)
 
-# Begins displays
-7segdisplay.begin()
+# Begins display
+ss7segdisplay.begin()
 alphadisplay.begin()
 
 # Loop for starting displays
-async def time():
+async def settime():
     # Sets Time
     utctime = datetime.utcnow()
     hour = utctime.hour + offset
@@ -34,16 +34,17 @@ async def time():
     second = utctime.second
 
     # Clear Display
-    7segdisplay.clear()
+    ssegdisplay.clear()
 
-    7segdisplay.set_colon(second % 2)
+    ssegdisplay.set_colon(second % 2)
     await asyncio.sleep(0.25)
+
 async def buttons():
     async if GPIO.input(18) == False:
         print('Left button pressed')
         await asyncio.sleep(0.02)
 
 asyncloop = asyncio.get_event_loop()
-tasks = [asyncloop.create_task(time()), asyncloop.create_task(buttons())]
+tasks = [asyncloop.create_task(settime()), asyncloop.create_task(buttons())]
 wait_tasks = asyncio.wait(tasks)
 asyncloop.run_forever()
